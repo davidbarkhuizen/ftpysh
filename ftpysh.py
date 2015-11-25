ae3da5b3e6b4df36a0578447c742f558d7d7724f
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-11-25 08:58:03 +0200

    changed parameter ordering; added *.*~ to .gitignore

--- ftpysh.py
+++ ftpysh.py
@@ -1,14 +1,17 @@
+2015 david barkhuizen
+
 import ftplib
 import argparse
 
 # -------------------------
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog='ftpysh = python ftp push')
+
+parser.add_argument("file")
 
 parser.add_argument("server")
 parser.add_argument("user")
 parser.add_argument("password")
-parser.add_argument("file")
 parser.add_argument("destination_root")
 
 args = parser.parse_args()
